8613dd3eb781c4dc5445ca25bab5c9fa86f09ea0
LawfulDickish <36271525+LawfulDickish@users.noreply.github.com>
2021-03-27 15:44:42 -0400

    merge cleanup 4/?
    hopefully the last

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -264,7 +264,7 @@ class TsuServer3:
 
     def save_miscdata(self):
         with open('config/data.yaml', 'w') as data:
-            json.dump(self.misc_data, data, indent=4)
+            yaml.dump(self.misc_data, data, indent=4)
 
     def load_backgrounds(self):
         """Load the backgrounds list from a YAML file."""
