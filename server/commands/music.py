d988966b271519207a1a44dacd0a73a28ba22e70
yemta <43356138+yemta@users.noreply.github.com>
2021-08-27 18:54:01 +0100

    Add checks to /play usage

--- server/commands/music.py
+++ server/commands/music.py
@@ -163,6 +163,19 @@ def ooc_cmd_play(client, arg):
         raise ArgumentError('You must specify a song.')
     if YOUTUBE_RE.search(arg):
         raise ArgumentError('You cannot use YouTube links. You may use direct links to MP3, Ogg, or M3U streams.')
+    if client.is_muted:
+        raise ArgumentError('You are muted by a moderator.')
+    if not client.is_dj:
+            raise ArgumentError('You were blockdj\'d by a moderator.')
+    if client.area.cannot_ic_interact(client):
+            raise ArgumentError(
+                "You are not on the area's invite list, and thus, you cannot change music!"
+            )
+    if client.change_music_cd():
+        if (len(client.area.clients) != 1):
+            raise ArgumentError(
+                f'You changed the song too many times. Please try again after {int(client.change_music_cd())} seconds.'
+            )
     client.area.play_music(arg, client.char_id, 0) #don't loop it
     client.area.add_music_playing(client, arg)
     client.area.add_to_musiclog(client, f'played {arg}')
