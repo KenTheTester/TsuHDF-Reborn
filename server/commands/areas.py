0212a5b9e7a71892f7a5565710f8aac772d8f387
yemta <43356138+yemta@users.noreply.github.com>
2022-02-05 00:00:35 +0000

    i lied
    midnight coder in stockings

--- server/commands/areas.py
+++ server/commands/areas.py
@@ -421,19 +421,27 @@ def ooc_cmd_uninvite(client, arg):
 def ooc_cmd_area_kick(client, arg):
     """
     Remove a user from the current area and move them to another area.
-    Usage: /area_kick <id> [area id]
     If no area id is entered, user will be kicked to area 0.
+    Usage: /area_kick <id> [area id]
+    Special cases:
+    - "afk" area kicks all users set to /afk.
+    - "*" area kicks everyone in the current area.
     """
     if not arg:
         raise ClientError(
             'You must specify a target. Use /area_kick <id> [area id]')
     arg = arg.split(' ')
-    if arg[0] == 'afk':
+    if arg[0].lower() == 'afk':
         trgtype = TargetType.AFK
         argi = arg[0]
+    elif arg[0] == '*':
+        trgtype = TargetType.ALL
+        argi = arg[0]
+        targets = [c for c in client.area.clients if c != client]
     else:
         trgtype = TargetType.ID
         argi = int(arg[0])
+    
     targets = client.server.client_manager.get_targets(client, trgtype,
                                                        argi, False)
     if targets:
